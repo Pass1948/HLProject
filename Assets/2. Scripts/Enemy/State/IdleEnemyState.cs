38914d2c73f21357eb0cba32f934150629122805
totalpsh <66562462+totalpsh@users.noreply.github.com>
2025-09-11 20:34:35 +0900

    [Feat] 에너미AI FSM 기반 구축 (#8)
    * [Feat] 에너미 구상중
    
    * [Feat] 에너미 관련 cs파일 생성
    
    * [Feat] 에너미 AI FSM 기반 닦아놓기

--- Assets/2. Scripts/Enemy/State/IdleEnemyState.cs
+++ Assets/2. Scripts/Enemy/State/IdleEnemyState.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleEnemyState : MonoBehaviour
+// 턴이 오기 전에 대기하고 있는 상태
+public class IdleEnemyState : BaseEnemyState
 {
-    // Start is called before the first frame update
-    void Start()
+    public IdleEnemyState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
     {
-        
+        Debug.Log("Idle : Enter");
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Excute()
     {
-        
+        Debug.Log("Idle : Excute");
+
+        // 대기중
+        // do nothing!!
+        // 턴 시작의 경우 Evaluate 전환
+        // or
+        // Hit 될 경우 Hit상태 전환
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Idle : Exit");
     }
 }
