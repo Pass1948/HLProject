9b52078a3742edd9d9d995aa3b95f82fe2cc4b86
Leejinyoung126 <shinyshines76@gmail.com>
2025-09-11 11:21:42 +0900

    [Fix]필요없는 코드와 스크립트제거

--- Assets/2. Scripts/Weapons/AttackTestController.cs
+++ Assets/2. Scripts/Weapons/AttackTestController.cs
@@ -11,16 +11,7 @@ public class AttackTestController : MonoBehaviour
 
     private Color bgNormal = new Color(0.6f, 1f, 0.6f, 1f);
     private Color bgSel = new Color(1f, 0.9f, 0.5f, 1f);
-
-    private TargetTest selectedTarget;
     private Button selectedAmmoBtn;
-    
-
-    //타겟지정
-    public void SelectTarget(TargetTest target)
-    {
-        selectedTarget = target;
-    }
 
     //탄환버튼 OnClick
     public void SelectAmmo(Button btn)
