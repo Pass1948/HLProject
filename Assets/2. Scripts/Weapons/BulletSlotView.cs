d7bbfcca4e3711d59bd08c4de80d6d98fe1e6897
Leejinyoung126 <shinyshines76@gmail.com>
2025-09-12 21:03:00 +0900

    [Feat}재장전 로직 구현

---
+++ Assets/2. Scripts/Weapons/BulletSlotView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletSlotView : MonoBehaviour
+{
+    public Image bulletBg;
+    public Ammo ammo;
+}
