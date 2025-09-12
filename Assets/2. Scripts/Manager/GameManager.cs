d3a0af4ab49fda1abc797482ad1298a3a2580183
LeeYoungSin <dldudtls100@gmail.com>
2025-09-12 17:06:17 +0900

    [Fix] MapManager를 GameManager에 연결 및 일부 동적할당 수정

--- Assets/2. Scripts/Manager/GameManager.cs
+++ Assets/2. Scripts/Manager/GameManager.cs
@@ -36,6 +36,9 @@ public class GameManager : MonoBehaviour
     private static PathPreviewManager pathPreviewManager;
     public static PathPreviewManager PathPreview => pathPreviewManager;
 
+    private static MapManager mapManager;
+    public static MapManager Map => mapManager;
+
     private void Awake()
     {
         if (instance != null) { Destroy(this); return; }
@@ -60,6 +63,7 @@ public class GameManager : MonoBehaviour
         eventManager = CreateChildManager<EventManager>("EventManager");
         turnBasedManager = CreateChildManager<TurnBasedManager>("TurnBasedManager");
         pathPreviewManager = CreateChildManager<PathPreviewManager>("PathPreviewManager");
+        mapManager = CreateChildManager<MapManager>("MapManager");
     }
     private T CreateChildManager<T>(string goName) where T : Component
     {
