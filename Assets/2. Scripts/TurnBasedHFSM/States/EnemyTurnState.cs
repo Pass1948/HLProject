1fb3b170ec85a27e5ed3ae6ed004f63eba5225dd
LeeYoungSin <dldudtls100@gmail.com>
2025-09-15 10:40:21 +0900

    [fix] 일부수정

--- Assets/2. Scripts/TurnBasedHFSM/States/EnemyTurnState.cs
+++ Assets/2. Scripts/TurnBasedHFSM/States/EnemyTurnState.cs
@@ -10,10 +10,10 @@ public class EnemyTurnState : BaseTurnState
     }
     public override void Tick(float dt)
     {
-        if ()// 몬스터 행동 체크 조건으로 실행
+       /* if ()// 몬스터 행동 체크 조건으로 실행
         {
             // ChangeState<IdleState>(); // 몬스터 행동 끝나면 IdleState로
             // ChangeState<ClearCheckState>(); //  플래이어 죽을시 ClearCheckState로
-        }
+        }*/
     }
 }
