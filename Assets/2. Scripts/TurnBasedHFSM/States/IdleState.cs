d0aebb1462d6b12e5df25ea2b05644a65f72be77
LeeYoungSin <dldudtls100@gmail.com>
2025-09-16 08:57:32 +0900

    [feat] 턴별 연출용 ui세팅

--- Assets/2. Scripts/TurnBasedHFSM/States/IdleState.cs
+++ Assets/2. Scripts/TurnBasedHFSM/States/IdleState.cs
@@ -9,12 +9,14 @@ public class IdleState : BaseTurnState
     public override void OnEnter() 
     {
         timer =  turnSetVlaue.resetTime;
+        GameManager.UI.OpenUI<PaseTurnUI>();
     }
     public override void Tick(float dt)
     {
         timer+= dt;
         if(timer>turnSetVlaue.turnDelayTime)
         {
+            GameManager.UI.CloseUI<PaseTurnUI>();
             ChangeState<PlayerTurnState>();
         }
     }
