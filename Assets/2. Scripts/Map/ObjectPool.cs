26fdfdccee0f098916b01b1fc51510c6828ed749
0time12 <onr55@naver.com>
2025-09-15 17:56:10 +0900

    [Feat] 오브젝트 풀링, 스폰 방식 변경

---
+++ Assets/2. Scripts/Map/ObjectPool.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> : MonoBehaviour
+{
+    public GameObject prefab;
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+    public void InitializePool(int initialCount)
+    {
+        for (int i = 0; i < initialCount; i++)
+        {
+            GameObject obj = Instantiate(prefab, transform);
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        if (pool.Count > 0)
+        {
+            GameObject obj = pool.Dequeue();
+            obj.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            GameObject newObj = Instantiate(prefab, transform);
+            newObj.SetActive(true);
+            return newObj;
+        }
+    }
+
+    public void ReturnObjectToPool(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
