1f42040f595d580948cafa7e7c52f465b0d39007
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-09-16 21:15:24 +0900

    [Fix] : 플레이어의 움직이는 정보 타일 수정
    정보를 받아 타일을 깔 때 타일 아이디가 터레인이 아니면 안깔게 했는데 그렇게 되다보니 플레이어의 좌표를 읽고 있는 타일에 걸려서 깔리지 않았다.

--- Assets/2. Scripts/Character/Movements/MovementController.cs
+++ Assets/2. Scripts/Character/Movements/MovementController.cs
@@ -15,15 +15,16 @@ public class MovementController : MonoBehaviour
     [SerializeField] private float groundY = 0f; // 그리드 셀 크기
 
     [Header("Movement Settings")]
+    [SerializeField] private int moveRange; // 이동 범위
     [SerializeField] private float moveTime = 0.2f;
 
     public Vector3Int _cellPosition; // 플레이어 현재 위치
     public bool _isMoving = false;  // 움직임 감지
-    public bool isPlayer=false;
-
-    
+    public bool isPlayer = false;
 
     private Pathfinding _pathfinding;
+
+    private BasePlayer basePlayer;
 
     private void OnEnable()
     {
@@ -33,15 +34,20 @@ public class MovementController : MonoBehaviour
     {
 
     }
+    private void Awake()
+    {
+        basePlayer = GetComponent<BasePlayer>();
 
+    }
 
     private void Start()
     {
-        GameManager.Data.playerData.playerMoveData.PlayerPos = _cellPosition;
+
+        tilemap = GameManager.Map.tilemap;
         // 플레이어 시작 위치를 타일의 중앙으로 설정
         _cellPosition = tilemap.WorldToCell(transform.position);
         transform.position = tilemap.GetCellCenterWorld(_cellPosition);
-
+        moveRange = basePlayer.playerModel.moveRange;
 
         // A* 알고리즘 초기화
         _pathfinding = new Pathfinding(tilemap);
@@ -50,30 +56,20 @@ public class MovementController : MonoBehaviour
     {
         GetCellPosition();
         //TODO: 마우스가 움직일 때마다 경로 미리보기(장보석,이영신)
-        //if (_isMoving == false)return;
-        if (TryGetMouseWorldOnGrid(out var mouseWorld))
-        {
-            var targetCell = tilemap.WorldToCell(mouseWorld);
-            if (targetCell != _cellPosition)
-            {
-                var path = _pathfinding.FindPath(_cellPosition, targetCell);
-                int moveRange = GameManager.Data.playerData.playerMoveData.MoveRange;
-                GameManager.Data.playerData.playerMoveData.PlayerPos = _cellPosition;
-                PlayerMoveRange(path, tilemap, moveRange);
-            }
-        }
 
-    }
+        //if (isPlayer == true)
+        //{
+        //    if (TryGetMouseWorldOnGrid(out var mouseWorld))
+        //    {
+        //        var targetCell = tilemap.WorldToCell(mouseWorld);
+        //        if (targetCell != _cellPosition)
+        //        {
+        //            var path = _pathfinding.FindPath(_cellPosition, targetCell);
 
-    public void Init(Tilemap tilemap)
-    {
-        this.tilemap = tilemap;
-        // 플레이어 시작 위치를 타일의 중앙으로 설정
-        _cellPosition = tilemap.WorldToCell(transform.position);
-        transform.position = tilemap.GetCellCenterWorld(_cellPosition);
-
-        // A* 알고리즘 초기화
-        _pathfinding = new Pathfinding(tilemap);
+        //            PlayerMoveRange(path, tilemap, moveRange);
+        //        }
+        //    }
+        //}
     }
 
 
@@ -81,8 +77,10 @@ public class MovementController : MonoBehaviour
     {
         GameManager.PathPreview.ShowPath(path, tilemap, moveRange);
     }
-
-
+    public void GetPosition(int x, int y)
+    {
+        _cellPosition = new Vector3Int(x, y, 0);
+    }
 
     private void OnMovementClick(InputValue value)
     {
@@ -95,13 +93,9 @@ public class MovementController : MonoBehaviour
         if (!value.isPressed) return;
 
         TryGetMouseWorldOnPlayer();
+        if (isPlayer == false) return;
         if (TryGetMouseWorldOnGrid(out var mouseWorld))
         {
-
-            if (!TryGetMouseWorldOnGrid(out var mouseWorld)) return;
-            
-            // 마우스 위치를 셀 위치로 변환
-
             OnclickInfo(mouseWorld);
         }
 
@@ -109,28 +103,25 @@ public class MovementController : MonoBehaviour
 
     public void OnclickInfo(Vector3 mouseWorld)
     {
-        if (isPlayer== false) return;
         // 마우스 위치를 셀 위치로 변환
         var targetCell = tilemap.WorldToCell(mouseWorld);
-
         // 위치의 변화가 없거나 같으면 무시
         if (targetCell == _cellPosition) return;
 
         // A* 알고리즘 경로 설정
         // _cellPosition : 시작 위치, targetCell : 목표 위치
         List<Vector3Int> path = _pathfinding.FindPath(_cellPosition, targetCell);
-
-        int moveRange = GameManager.Data.playerData.playerMoveData.MoveRange;
+        Debug.Log($"Path Count : {_cellPosition}");
 
         if (path.Count > moveRange)
         {
+            isPlayer = false;
             return;
         }
-        // maxRange 보다 작거나 같을 때만 이동
-        var maxRange = GameManager.Data.playerData.playerMoveData.MoveRange;
-
         StopAllCoroutines();
         StartCoroutine(FollowPath(path));
+
+        isPlayer = false;
     }
 
     /// <summary>
@@ -144,44 +135,21 @@ public class MovementController : MonoBehaviour
         // 레이를 쏴서 테그가 맵이 아니면 무시
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-
-            //if (hit.collider.gameObject.CompareTag("MoveRange"))
-            //{
-            world = hit.point;
-            Debug.Log($" {world}");
-            return true;
-            //    }
-            //    else
-            //    {
-            //        world = default;
-            //        return false;
-            //    }
-            //}
+            if (hit.collider.gameObject.CompareTag("TileMap"))
+            {
+                world = hit.point;
+                return true;
+            }
+            else
+            {
+                world = default;
+                return false;
+            }
         }
         world = default;
         return false;
     }
 
-
-    // 플레이어 클릭시 나오는 행동 메서드(작성자: 이영신)
-    private bool TryGetMouseWorldOnMoveRange()
-    {
-        var mousePos = Mouse.current.position.ReadValue();
-        Ray ray = Camera.main.ScreenPointToRay(mousePos);
-        // 레이를 쏴서 테그가 맵이 아니면 무시
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            if (hit.collider.gameObject.CompareTag("MoveRange"))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        return false;
-    }
 
     // 플레이어 클릭시 나오는 행동 메서드(작성자: 이영신)
     private void TryGetMouseWorldOnPlayer()
@@ -193,20 +161,15 @@ public class MovementController : MonoBehaviour
             if (hit.collider.gameObject.CompareTag("Player"))
             {
                 // TODO: 플레이어 클릭시 이동범위 확인할수있음
-
-                GameManager.Map.PlayerUpdateRange();
-
+                GameManager.Map.PlayerUpdateRange(_cellPosition, moveRange);
+                Debug.Log("Player Click True");
                 isPlayer = true;
-
             }
             else
             {
                 // TODO: 다른곳 클릭시 이동범위 사라짐
-
+                Debug.Log("Player Click False");
                 GameManager.Map.ClearPlayerRange();
-
-                isPlayer = false;
-
             }
         }
     }
@@ -242,7 +205,10 @@ public class MovementController : MonoBehaviour
 
         transform.position = end;
         _cellPosition = targetCell;
-        GameManager.Data.playerData.playerMoveData.PlayerPos = _cellPosition;
+
+        GameManager.Map.UpdateObjectPosition((int)start.x, (int)start.y, (int)_cellPosition.x, (int)_cellPosition.y, TileID.Player);
+        Debug.Log($"이건 못참지 {GameManager.Map.GetPlayerPosition()}");
+
         _isMoving = false;
     }
 }
