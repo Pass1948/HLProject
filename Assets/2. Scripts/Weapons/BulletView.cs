c1fa6bf9f5042cc263f106a325fb6485046aaa7c
Leejinyoung126 <shinyshines76@gmail.com>
2025-09-17 09:34:14 +0900

    [Fix] 슬롯뷰랑 라벨뷰 합치기 + 코드정리 (#45)
    * [Test] : 게임오브젝트 생성
    
    풀리퀘스트 테스트를 위해 테스트JBS 씬에 테스트 오브젝트 생성
    
    * [Fix]이름변경
    
    * [Feat] : 유니티 씬 오브젝트 추가
    
    테스트 오브젝트 추가
    
    * [Fix,Docs] :  불필요한 코드 제거, 주석 추가
    
    플레이어의 움직임에서 필요없는 로직들을 지움,
    노드 스크립트의 변수에 이해를 돕기 위한 주석 추가
    
    * [Feat] : 마우스 올리면 위치에 타일 깔기
    
    마우스의 위치를 가지고 와서 그 위치와 플레이어의 위치 사이에 프리팹 해 놓은 타일을 깔아준다
    
    * Merge branch 'JBS' into Develop
    
    * [Fix] : 버그 패치
    
    깃 버그 패치
    
    * [Feat] 시작후 탄환 프리팹 6발 생성
    
    * Update CharacterMovementController.cs
    
    * [Fix]명칭변경
    
    * [Feat] 데이터 CS 추가
    
    플레이어 데이터 추가
    
    * [Fix] : cs 파일 이름 수정
    
    cs파일 내부 cs 이름 수정
    
    * [Feat] : 데이터 관리 추가
    
    데이터 매니저, 데이터 관리 cs 추가
    
    * [Feat}재장전 로직 구현
    
    * [Fix]탄을 쏘면 Discard로 이동하게함
    
    * [Chore] 화면에서 구분하는 빈 오브젝트 생성
    
    * [Fix] 탄환 데이터 Rank부분을 숫자로 구성
    
    * [Feat]탄환 숫자붙이기와 재장전 매커니즘 변경
    
    * [Add] 폰트 잘 구웠습니다
    
    * [Fix]재장전 버튼도 리스너 달기
    
    * [Feat] 탄환선택안했으면 발사버튼 비활성화
    
    재장전도 리스너화
    
    * [Fix] 기존 뷰 부분 합치기, 코드 정리
    
    ---------
    
    Co-authored-by: 장보석 (Jang Bo Seok) <heeno7700@gmail.com>

---
+++ Assets/2. Scripts/Weapons/BulletView.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BulletView : MonoBehaviour
+{
+    public Image bulletBg;//선택 표시용 배경
+    [SerializeField] private TMP_Text label;//D2/C6/H5등등 표기
+
+    public Ammo ammo;
+
+    public void RefreshLabel()
+    {
+        if (!label)
+        {
+            return;
+        }
+        label.text = ammo != null ? SuitLetter(ammo.suit) + ammo.rank : string.Empty;
+    }
+
+    public void SetBgColor(Color c)
+    {
+        if (bulletBg)
+        {
+            bulletBg.color = c;
+        }
+    }
+
+    private static string SuitLetter(Suit s)
+    {
+        switch (s)
+        {
+            case Suit.Spade: return "S";
+            case Suit.Heart: return "H";
+            case Suit.Diamond: return "D";
+            case Suit.Club: return "C";
+            default: return "?";
+        }
+    }
+}
+
