8860e0506826fdee657c4b727501326430745613
Leejinyoung126 <shinyshines76@gmail.com>
2025-09-18 16:11:51 +0900

    [Feat] 페이즈 UI의 상태반영 추가

---
+++ Assets/2. Scripts/UI/PhaseViewUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class PhaseViewUI : MonoBehaviour
+{
+    [SerializeField] private TurnBasedManager turn;
+    [SerializeField] private TMP_Text phaseViewText;
+
+    private string nowPhase;
+
+    private void Awake()
+    {
+        if(!turn && GameManager.TurnBased != null)
+        {
+            turn = GameManager.TurnBased;
+        }
+    }
+
+    private void Start()
+    {
+        Refresh(true); //시작시 1회 갱신
+    }
+
+    private void Update()
+    {
+        Refresh(false);
+    }
+
+    private void Refresh(bool force)
+    {
+        if(!phaseViewText)
+        {
+            return;
+        }
+
+        string current = (turn != null) ? turn.GetState() : "None";
+
+        if(force || current != nowPhase)
+        {
+            phaseViewText.text = ($"{current}");
+        }
+    }
+}
