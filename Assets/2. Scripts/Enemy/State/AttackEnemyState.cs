359c050ddbbc42c7f8c9b12c019d6b55d4f45e52
totalpsh <66562462+totalpsh@users.noreply.github.com>
2025-09-18 16:26:05 +0900

    [feat] 에너미 공격전 공격범위 미리보기 추가 (#59)
    * [Feat] 에너미 구상중
    
    * [Feat] 에너미 관련 cs파일 생성
    
    * [Feat] 에너미 AI FSM 기반 닦아놓기
    
    * [Feat] FSM 상태 내부 로직 구현중
    
    좌표주세요..
    
    * [Feat] 아.. 아직 FSM 로직 구현중
    
    타일맵 주세요... 좌표 주세요...
    
    * [FIx] 괄호 하나 빠져서 수정
    
    * [Fix] 맵매니저에 중복 변수 삭제
    
    * [chore] 타일id constants 추가 Test씬에 타일 동적생성 추가
    
    * [feat] 에너미 상태 전환 구현
    
    * [fix] 에너미 자꾸 변경되서 커밋함
    
    * [fix] 프리펩 잘못 불러오고 있는 문제 해결
    
    TurnBaseManager Awake set 메서드 주석 처리
    
    * [feat] DataManager 구현 CSV 파일 추가
    
    * [feat] 에너미 MVC 구축중
    
    * [feat] 에너미 스폰 보충 작업
    
    * [feat] 에너미 스폰시 초기화 가능
    
    * [feat] Enemy MVC 구현
    
    * [fix] 에너미 생성문제 해결중 ..
    
    * [fix] enemy 생성 수정 완
    
    * [feat] 에너미 움직임 수정 & 패스파인딩에 타일맵 갱신 메서드 추가
    
    * [feat] 에너미 길 미리보기 구현중 풀 받을려고 커밋함
    
    * [feat] Enemy정보 UI 구현
    
    * [chore] 에셋 받기 위해 커밋
    
    * [feat] SaveLoadManager 파일 추가 (미구현)
    
    * [feat] 데미지 처리 구현!!
    
    * [feat] 발차기 메서드 추가, 결과 UI 프리펩 화 시킴
    
    TODO 주석으로 찾아주시면 볼수있습니다.
    
    * [feat] 에너미 공격전 공격범위 미리보기 구현
    
    ---------
    
    Signed-off-by: LeeYoungSin <129037485+Pass1948@users.noreply.github.com>
    Co-authored-by: LeeYoungSin <129037485+Pass1948@users.noreply.github.com>

--- Assets/2. Scripts/Enemy/State/AttackEnemyState.cs
+++ Assets/2. Scripts/Enemy/State/AttackEnemyState.cs
@@ -1,20 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class AttackEnemyState : BaseEnemyState
 {
+    private int minRange => controller.minAttackRange;
+    private int maxRange => controller.maxAttackRange;
+
+    private List<GameObject> previewTiles = new List<GameObject>();
+
     public AttackEnemyState(EnemyStateMachine stateMachine, EnemyController controller, EnemyAnimHandler animHandler) : base(stateMachine, controller, animHandler) { }
 
     public override void Enter()
     {
         Debug.Log("Attack : Enter");
 
-        GameManager.Unit.ChangeHealth(GameManager.Unit.Player.playerModel, controller.model.attack);
+        ShowAttackPreview(controller.GridPos, minRange, maxRange);
 
-        animHandler.OnAttack();
-
-        stateMachine.ChangeState(stateMachine.EndState);
+        controller.StartCoroutine(AttackRoutine());
     }
 
     public override void Excute()
@@ -27,5 +32,59 @@ public class AttackEnemyState : BaseEnemyState
     {
         Debug.Log("Attack : Exit");
 
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        Vector2Int playerPos2D = GameManager.Map.GetPlayerPosition();
+        Vector3Int playerPos = new Vector3Int(playerPos2D.x, playerPos2D.y, 0);
+
+        int dist = Mathf.Abs(controller.GridPos.x - playerPos.x) + Math.Abs(controller.GridPos.y - playerPos.y);
+
+        if (dist >= minRange && dist <= maxRange)
+        {
+            GameManager.Unit.ChangeHealth(GameManager.Unit.Player.playerModel, controller.model.attack);
+        }
+
+        ClearPreveiwTiles();
+
+        animHandler.OnAttack();
+
+        stateMachine.ChangeState(stateMachine.EndState);
+    }
+
+    private void ShowAttackPreview(Vector3Int center, int minRange, int maxRange)
+    {
+        ClearPreveiwTiles();
+
+        Tilemap overlayTileMap = GameManager.Map.moveInfoTilemap;
+        overlayTileMap.ClearAllTiles();
+        for (int x = -maxRange; x <= maxRange; x++)
+        {
+            for (int y = -maxRange; y <= maxRange; y++)
+            {
+                int dist = Mathf.Abs(x) + Mathf.Abs(y);
+                if (dist >= minRange && dist <= maxRange)
+                {
+                    Vector3Int cell = new Vector3Int(center.x + x, center.y + y, 0);
+                    GameObject tile = GameManager.Resource.Create<GameObject>(Path.Map + "RedTile");
+                    //overlayTileMap.SetTile(cell, overlayTile);
+                    Vector3 worldPos = overlayTileMap.GetCellCenterWorld(cell);
+                    tile.transform.position = worldPos;
+                    previewTiles.Add(tile);
+                }
+            }
+        }
+    }
+
+    private void ClearPreveiwTiles()
+    {
+        foreach (var obj in previewTiles)
+        {
+            GameObject.Destroy(obj);
+        }
+        previewTiles.Clear();
     }
 }
