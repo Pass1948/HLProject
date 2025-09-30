631a979ef8058e66b6f74bbd11d6f700c5e95ffd
LeeYoungSin <dldudtls100@gmail.com>
2025-09-30 22:21:30 +0900

    [fix] 일부수정

--- Assets/2. Scripts/UI/MainUI.cs
+++ Assets/2. Scripts/UI/MainUI.cs
@@ -90,6 +90,7 @@ public class MainUI : BaseUI
     private void OnEnable()
     {
         reloadBtnObj.ReloadChange += OnReloadChanged;
+        stageInfoText.text = $"Stage {(GameManager.SaveLoad.nextSceneIndex+1).ToString()}";
     }
     private void OnDisable()
     {
