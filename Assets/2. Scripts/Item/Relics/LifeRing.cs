8c7bc709aec06e77bb5ef4d1dd94a172fcc17a6c
LeeYoungSin <dldudtls100@gmail.com>
2025-10-28 20:52:38 +0900

    [fix] 아이템 수정

--- Assets/2. Scripts/Item/Relics/LifeRing.cs
+++ Assets/2. Scripts/Item/Relics/LifeRing.cs
@@ -1,23 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Bible : HealthItem
+public class LifeRing : HealthItem
 {
     protected override void OnEnable()
     {
-        base.OnEnable();   
-        AddHealth(relicItems, 3005);
+        base.OnEnable();
+        AddHealth(relicItems, 3006);
     }
 
     private void OnDisable()
     {
-        RemoveHealth(relicItems, 3005);
+        RemoveHealth(relicItems, 3006);
     }
     private void OnDestroy()
     {
-        RemoveHealth(relicItems, 3005);
+        RemoveHealth(relicItems, 3006);
     }
-
 }
