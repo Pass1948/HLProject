b2b1e2836fd6ca0e3ec5a76b073a1b667e8c4815
LeeYoungSin <dldudtls100@gmail.com>
2025-10-29 18:29:35 +0900

    [fix] 아이템 수정

--- Assets/2. Scripts/Item/Relics/HealthFood.cs
+++ Assets/2. Scripts/Item/Relics/HealthFood.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FullPoket : MulliganItem
+public class HealthFood : MulliganItem
 {
     protected override void OnEnable()
     {
