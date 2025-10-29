34ff96de8c663ca60de5b3504b367634a794e03e
LeeYoungSin <dldudtls100@gmail.com>
2025-10-29 12:23:43 +0900

    [fix] 일부수정

--- Assets/2. Scripts/Manager/ShopManager.cs
+++ Assets/2. Scripts/Manager/ShopManager.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
 using Random = UnityEngine.Random;
 
 public class ShopManager : MonoBehaviour
