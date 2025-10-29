aebb75443bc7b7694d547731adfe5ff238847d8d
totalpsh <totalpsh6@gmail.com>
2025-10-29 16:36:09 +0900

    [fix] 적 이동 회전 적용 / 아직 보스는 마이클 잭슨임

--- Assets/2. Scripts/Units/Enemy/State/MoveEnemyState.cs
+++ Assets/2. Scripts/Units/Enemy/State/MoveEnemyState.cs
@@ -29,10 +29,8 @@ public class MoveEnemyState : BaseEnemyState
         if (range > 0)
         {
             Vector3 nextPos = GameManager.Map.tilemap.GetCellCenterWorld(path[0]);
-            GameObject dummyTarget = new GameObject("MoveLookTarget");
-            dummyTarget.transform.position = nextPos;
-            
-            animHandler.OnMove(true, dummyTarget.transform);
+
+            animHandler.OnMove(true, nextPos);
             controller.StartCoroutine(MoveAnim(path.GetRange(0, range)));
         }
         else
@@ -49,14 +47,7 @@ public class MoveEnemyState : BaseEnemyState
 
     public override void Exit()
     {
-        Vector3 finalPos = controller.transform.position + controller.transform.forward;
-    
-        // 임시 타겟 생성 (바라보는 방향 유지용)
-        GameObject dummy = new GameObject("LastLookTarget");
-        dummy.transform.position = finalPos;
-        
-        animHandler.OnMove(false, dummy.transform);
-        GameObject.Destroy(dummy, 0.1f);
+        animHandler.OnMove(false, controller.transform.position);
         GameManager.Map.ClearPlayerRange();
     }
 
