232ec83ae7a82d100722828c7aeeac2bed7905e6
LeeYoungSin <dldudtls100@gmail.com>
2025-10-30 10:47:59 +0900

    [fix] 일부수정

--- Assets/2. Scripts/UI/ResultUI.cs
+++ Assets/2. Scripts/UI/ResultUI.cs
@@ -50,10 +50,6 @@ public class ResultUI : BaseUI
         {
             overUI.CloseUI();
             clearUI.OpenUI();
-            Analytics.CustomEvent("game_clear_popup", new Dictionary<string, object> // TODO : game_clear_popup
-            {
-                { "onScreen", "게임 클리어 팝업 출력" },
-            });
         }
         else if (result == ResultType.Over)
         {
@@ -84,6 +80,10 @@ public class ResultUI : BaseUI
         }
         else if (result == ResultType.GameClear)
         {
+            Analytics.CustomEvent("game_clear_popup", new Dictionary<string, object> // TODO : game_clear_popup
+            {
+                { "onScreen", "게임 클리어 팝업 출력" },
+            });
             if (GameManager.TurnBased.turnSettingValue.isTutorial == true) return;
             gameClearUI.SetActive(true);
             tutorialUI1.SetActive(false);
